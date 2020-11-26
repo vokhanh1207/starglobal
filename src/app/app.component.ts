b16eccfb9a59b3b8f5967d679a1afdd4b92ffe67
vokhanh1207 <vokhanh1207@gmail.com>
2020-11-26 19:35:54 +0800

    starglobal: finish chalenge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'starglobal-challenge';
+  squareX = 0;
+  squareY = 0;
+  speed = 0.5;
+
+  constructor() {}
+
+  onClick(event: MouseEvent): void {
+    // the center of the square should coincide with the mouse,
+    // 25px is the offset
+    this.squareX = event.offsetX - 25;
+    this.squareY = event.offsetY - 25;
+  }
 }
